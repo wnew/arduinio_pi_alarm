9ae19352f88d5c430c10e9a438b329d10c1dcf12
wnew <wesley@ska.ac.za>
2016-11-23 14:50:54 +0200

    updated the python to take in cmd line arguments
    you can now specify the users using the command:
    python run.py -i <user>
    this will select a different list of sensors for each user

--- run.py
+++ run.py
@@ -2,6 +2,7 @@ import sys
 import time
 import serial
 import logging
+from optparse import OptionParser
 
 
 class alarm(object):
@@ -16,27 +17,33 @@ class alarm(object):
   logger.addHandler(fh)
 
   # a list of all the sensors in the system
-  # the amount must match the length of the length of the string coming from the arduino
-  sensors = [{'name': 'sliding door',       'pos':  0, 'pin':  2, 'state': 1, 'triggered':False, 'stay':True},
-             {'name': 'front door',         'pos':  1, 'pin':  3, 'state': 1, 'triggered':False, 'stay':True},
-             {'name': 'large garage door',  'pos':  2, 'pin':  4, 'state': 1, 'triggered':False, 'stay':True},
-             {'name': 'back door',          'pos':  3, 'pin':  5, 'state': 1, 'triggered':False, 'stay':True},
-             {'name': 'garage door',        'pos':  4, 'pin':  6, 'state': 1, 'triggered':False, 'stay':True},
-             {'name': 'lounge side door',   'pos':  5, 'pin':  7, 'state': 1, 'triggered':False, 'stay':True},
-             {'name': 'wooden gate',        'pos':  6, 'pin':  8, 'state': 1, 'triggered':False, 'stay':True},
-             {'name': 'main gate',          'pos':  7, 'pin':  9, 'state': 1, 'triggered':False, 'stay':True},
-             {'name': 'beam',               'pos': 12, 'pin': 14, 'state': 1, 'triggered':False, 'stay':True},
-             {'name': 'beam tamper',        'pos': 13, 'pin': 15, 'state': 1, 'triggered':False, 'stay':True},
-             {'name': 'kitchen pir',        'pos': 14, 'pin': 16, 'state': 1, 'triggered':False, 'stay':False},
-             {'name': 'kitchen pir tamper', 'pos': 15, 'pin': 17, 'state': 1, 'triggered':False, 'stay':True},
-             {'name': 'lounge pir',         'pos': 16, 'pin': 18, 'state': 1, 'triggered':False, 'stay':False},
-             {'name': 'lounge pir tamper',  'pos': 17, 'pin': 19, 'state': 1, 'triggered':False, 'stay':True},
-             {'name': 'garage pir',         'pos': 18, 'pin': 20, 'state': 1, 'triggered':False, 'stay':False},
-             {'name': 'garage pir tamper',  'pos': 19, 'pin': 21, 'state': 1, 'triggered':False, 'stay':True},
-             {'name': 'fence set',          'pos': 14, 'pin': 17, 'state': 1, 'triggered':False, 'stay':False},
-             {'name': 'alarm set',          'pos': 15, 'pin': 18, 'state': 1, 'triggered':False, 'stay':False},
-             {'name': 'panic button',       'pos': 16, 'pin': 19, 'state': 1, 'triggered':False, 'stay':False},
-             {'name': 'mystery missing sensor',   'pos': 17, 'pin': 20, 'state': 1, 'triggered':False, 'stay':False}]
+  # the amount of sensors must match the length of the string coming from the arduino
+  sensors_wes = [{'name': 'sliding door',       'pos':  0, 'pin':  2, 'state': 1, 'triggered':False, 'stay':True},
+                 {'name': 'front door',         'pos':  1, 'pin':  3, 'state': 1, 'triggered':False, 'stay':True},
+                 {'name': 'large garage door',  'pos':  2, 'pin':  4, 'state': 1, 'triggered':False, 'stay':True},
+                 {'name': 'back door',          'pos':  3, 'pin':  5, 'state': 1, 'triggered':False, 'stay':True},
+                 {'name': 'garage door',        'pos':  4, 'pin':  6, 'state': 1, 'triggered':False, 'stay':True},
+                 {'name': 'lounge side door',   'pos':  5, 'pin':  7, 'state': 1, 'triggered':False, 'stay':True},
+                 {'name': 'wooden gate',        'pos':  6, 'pin':  8, 'state': 1, 'triggered':False, 'stay':True},
+                 {'name': 'main gate',          'pos':  7, 'pin':  9, 'state': 1, 'triggered':False, 'stay':True},
+                 {'name': 'beam',               'pos': 12, 'pin': 14, 'state': 1, 'triggered':False, 'stay':True},
+                 {'name': 'beam tamper',        'pos': 13, 'pin': 15, 'state': 1, 'triggered':False, 'stay':True},
+                 {'name': 'kitchen pir',        'pos': 14, 'pin': 16, 'state': 1, 'triggered':False, 'stay':False},
+                 {'name': 'kitchen pir tamper', 'pos': 15, 'pin': 17, 'state': 1, 'triggered':False, 'stay':True},
+                 {'name': 'lounge pir',         'pos': 16, 'pin': 18, 'state': 1, 'triggered':False, 'stay':False},
+                 {'name': 'lounge pir tamper',  'pos': 17, 'pin': 19, 'state': 1, 'triggered':False, 'stay':True},
+                 {'name': 'garage pir',         'pos': 18, 'pin': 20, 'state': 1, 'triggered':False, 'stay':False},
+                 {'name': 'garage pir tamper',  'pos': 19, 'pin': 21, 'state': 1, 'triggered':False, 'stay':True},
+                 {'name': 'fence set',          'pos': 14, 'pin': 17, 'state': 1, 'triggered':False, 'stay':False},
+                 {'name': 'alarm set',          'pos': 15, 'pin': 18, 'state': 1, 'triggered':False, 'stay':False},
+                 {'name': 'panic button',       'pos': 16, 'pin': 19, 'state': 1, 'triggered':False, 'stay':False},
+                 {'name': 'missing sensor',     'pos': 17, 'pin': 20, 'state': 1, 'triggered':False, 'stay':False}]
+
+  # a list of all the sensors in the system
+  # the amount of sensors must match the length of the string coming from the arduino
+  sensors_paul = [{'name': 'sliding door',       'pos':  0, 'pin':  2, 'state': 1, 'triggered':False, 'stay':True},
+                  {'name': 'front door',         'pos':  1, 'pin':  3, 'state': 1, 'triggered':False, 'stay':True},
+                  {'name': 'large garage door',  'pos':  2, 'pin':  4, 'state': 1, 'triggered':False, 'stay':True}]
 
   # alarm states
   DISARMED  = 0
@@ -46,35 +53,50 @@ class alarm(object):
   alarm_state = DISARMED
 
   # set the correct serial port for the OS
-  if sys.plaform.startsWith('linux'):
+  if sys.platform.startswith('linux'):
     ser = serial.Serial(
-      port='/dev/ttyACM0',
-      baudrate=115200,
-      parity=serial.PARITY_NONE
-    )
-  # assume windows
+      port = '/dev/ttyACM0',
+      baudrate = 115200,
+      parity = serial.PARITY_NONE)
+  # else assume windows
   else:
     ser = serial.Serial(
-      port='COM1',
-      baudrate=115200,
-      parity=serial.PARITY_NONE
-    )
+      port = 'COM1',
+      baudrate = 115200,
+      parity = serial.PARITY_NONE)
 
 
-  def __init__(self):
+  def __init__(self, argv):
     """ init function : starts the loop to poll the serial interface """
+    self.manageArgs(argv)
+    self.logger.info('Starting up alarm system')
     self.ser.isOpen()
     while 1:
-        data = self.getSerialData()
+      data = self.getSerialData()
+
+
+  def manageArgs(self, argv):
+    """ reads the cmd line parameters and configures the script accordingly """
+    p = OptionParser()
+    p.set_usage('run.py [options]')
+    p.set_description(__doc__) 
+    p.add_option('-u', '--user', dest='user', action='store',
+        help='Specify the user') 
+
+    opts, args = p.parse_args(sys.argv[1:])
+    if opts.user == 'paul':
+      self.sensors = self.sensors_paul
+    else:
+      self.sensors = self.sensors_wes
 
 
   def getSerialData(self):
-  """ the function that does the work polling the serial interface """
+    """ polls the serial interface and checks if any sensors changed state """
     out = ''
     while self.ser.inWaiting() > 0:
       out += self.ser.readline()
       out = out.rstrip('\n\r')
-      if (out != "") and (len(out) == 22) and out.startswith('s') and out.endswith('e'):
+      if (out != "") and (len(out) == len(self.sensors)) and out.startswith('s') and out.endswith('e'):
         out = out[out.find('s')+1:out.find('e')]
         for sensor in self.sensors:
           if int(sensor['state']) != int(out[sensor['pos']]):
@@ -83,5 +105,10 @@ class alarm(object):
             self.logger.info(sensor['name'] + ' ' + str(sensor['state']))
 
 
+  def checkAlarmState(self):
+    """ checks the alarm state and decides whether or not to sound the siren """
+    pass
+
+
 # run the code above
-alarm()
+alarm(sys.argv)
