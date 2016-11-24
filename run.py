499a0869cd481aa0efc7a548040d523fc35734cb
wnew <wesley@ska.ac.za>
2016-11-24 18:01:12 +0200

    added support for config file
    - also added an example config file
    - the script can now be run as follows:
    - python run.py -c alarm.conf

--- run.py
+++ run.py
@@ -3,6 +3,7 @@ import time
 import serial
 import logging
 from optparse import OptionParser
+from ConfigParser import SafeConfigParser
 
 
 class alarm(object):
@@ -10,40 +11,10 @@ class alarm(object):
 
   logger = logging.getLogger(__name__)
   logger.setLevel(logging.INFO)
-
   fh = logging.FileHandler('sensor_log')
   formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
   fh.setFormatter(formatter)
   logger.addHandler(fh)
-
-  # a list of all the sensors in the system
-  # the amount of sensors must match the length of the string coming from the arduino
-  sensors_wes = [{'name': 'sliding door',       'pos':  0, 'pin':  2, 'state': 1, 'triggered':False, 'stay':True},
-                 {'name': 'front door',         'pos':  1, 'pin':  3, 'state': 1, 'triggered':False, 'stay':True},
-                 {'name': 'large garage door',  'pos':  2, 'pin':  4, 'state': 1, 'triggered':False, 'stay':True},
-                 {'name': 'back door',          'pos':  3, 'pin':  5, 'state': 1, 'triggered':False, 'stay':True},
-                 {'name': 'garage door',        'pos':  4, 'pin':  6, 'state': 1, 'triggered':False, 'stay':True},
-                 {'name': 'lounge side door',   'pos':  5, 'pin':  7, 'state': 1, 'triggered':False, 'stay':True},
-                 {'name': 'wooden gate',        'pos':  6, 'pin':  8, 'state': 1, 'triggered':False, 'stay':True},
-                 {'name': 'main gate',          'pos':  7, 'pin':  9, 'state': 1, 'triggered':False, 'stay':True},
-                 {'name': 'beam',               'pos': 12, 'pin': 14, 'state': 1, 'triggered':False, 'stay':True},
-                 {'name': 'beam tamper',        'pos': 13, 'pin': 15, 'state': 1, 'triggered':False, 'stay':True},
-                 {'name': 'kitchen pir',        'pos': 14, 'pin': 16, 'state': 1, 'triggered':False, 'stay':False},
-                 {'name': 'kitchen pir tamper', 'pos': 15, 'pin': 17, 'state': 1, 'triggered':False, 'stay':True},
-                 {'name': 'lounge pir',         'pos': 16, 'pin': 18, 'state': 1, 'triggered':False, 'stay':False},
-                 {'name': 'lounge pir tamper',  'pos': 17, 'pin': 19, 'state': 1, 'triggered':False, 'stay':True},
-                 {'name': 'garage pir',         'pos': 18, 'pin': 20, 'state': 1, 'triggered':False, 'stay':False},
-                 {'name': 'garage pir tamper',  'pos': 19, 'pin': 21, 'state': 1, 'triggered':False, 'stay':True},
-                 {'name': 'fence set',          'pos': 14, 'pin': 17, 'state': 1, 'triggered':False, 'stay':False},
-                 {'name': 'alarm set',          'pos': 15, 'pin': 18, 'state': 1, 'triggered':False, 'stay':False},
-                 {'name': 'panic button',       'pos': 16, 'pin': 19, 'state': 1, 'triggered':False, 'stay':False},
-                 {'name': 'missing sensor',     'pos': 17, 'pin': 20, 'state': 1, 'triggered':False, 'stay':False}]
-
-  # a list of all the sensors in the system
-  # the amount of sensors must match the length of the string coming from the arduino
-  sensors_paul = [{'name': 'sliding door',       'pos':  0, 'pin':  2, 'state': 1, 'triggered':False, 'stay':True},
-                  {'name': 'front door',         'pos':  1, 'pin':  3, 'state': 1, 'triggered':False, 'stay':True},
-                  {'name': 'large garage door',  'pos':  2, 'pin':  4, 'state': 1, 'triggered':False, 'stay':True}]
 
   # alarm states
   DISARMED  = 0
@@ -55,7 +26,7 @@ class alarm(object):
   # set the correct serial port for the OS
   if sys.platform.startswith('linux'):
     ser = serial.Serial(
-      port = '/dev/ttyACM0',
+      port = '/dev/ttyUSB0',
       baudrate = 115200,
       parity = serial.PARITY_NONE)
   # else assume windows
@@ -69,25 +40,31 @@ class alarm(object):
   def __init__(self, argv):
     """ init function : starts the loop to poll the serial interface """
     self.manageArgs(argv)
+
     self.logger.info('Starting up alarm system')
+
+    print self.sensors.sections()
     self.ser.isOpen()
     while 1:
       data = self.getSerialData()
+
+
+  def manageConfig(self, config_file = 'alarm.conf'):
+    """ reads the configuration from the file and gets all the sensors """
+    self.parser = SafeConfigParser()
+    self.parser.read(config_file)
+    self.sensors = self.parser
 
 
   def manageArgs(self, argv):
     """ reads the cmd line parameters and configures the script accordingly """
     p = OptionParser()
     p.set_usage('run.py [options]')
-    p.set_description(__doc__) 
-    p.add_option('-u', '--user', dest='user', action='store',
-        help='Specify the user') 
-
+    p.set_description(__doc__)
+    p.add_option('-c', '--config', dest='config_file', action='store',
+        help='Specify the configuration file. Default: alarm.conf') 
     opts, args = p.parse_args(sys.argv[1:])
-    if opts.user == 'paul':
-      self.sensors = self.sensors_paul
-    else:
-      self.sensors = self.sensors_wes
+    self.manageConfig(opts.config_file)
 
 
   def getSerialData(self):
@@ -96,13 +73,17 @@ class alarm(object):
     while self.ser.inWaiting() > 0:
       out += self.ser.readline()
       out = out.rstrip('\n\r')
-      if (out != "") and (len(out) == len(self.sensors)) and out.startswith('s') and out.endswith('e'):
+      print out
+
+      if (out != "") and (len(out) == len(self.sensors.sections())+2) and out.startswith('s') and out.endswith('e'):
         out = out[out.find('s')+1:out.find('e')]
-        for sensor in self.sensors:
-          if int(sensor['state']) != int(out[sensor['pos']]):
-            sensor['state'] = int(out[sensor['pos']])
-            print sensor['name'] + str(sensor['state'])
-            self.logger.info(sensor['name'] + ' ' + str(sensor['state']))
+        print out
+        for i, sensor in enumerate(self.sensors.sections()):
+          if int(self.sensors.get(sensor, 'state')) != int(out[i]):
+            self.sensors.set(sensor, 'state', str(out[i]))
+            print self.sensors.get(sensor, 'name') + self.sensors.get(sensor, 'state')
+            self.logger.info(elf.sensors.get(sensor, 'name') + ' ' + self.sensors.get(sensor, 'state'))
+            self.checkAlarmState()
 
 
   def checkAlarmState(self):
